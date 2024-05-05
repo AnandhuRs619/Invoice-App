d8652bf8e13fdf8e48ff531cbd2584238a0c5d3f
AnandhuRs619 <ananthurs619@gmail.com>
2024-05-05 23:34:27 +0530

    ui Design Started with Mui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import { Homepage } from './pages/Homepage'
+// import './App.css';
+function App() {
+ 
+  return (
+    <>
+     <Homepage/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+// import Header from "../components/common/Header"
+import Navbar from "../components/common/NavBar"
+
+
+export const Homepage = () => {
+  return (
+    <>
+    
+    <Navbar/>
+    <div>Homepage</div>
+    </>
+  )
+}
